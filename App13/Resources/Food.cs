12c758f84d11cc59b1930a069d12509a0200dc41
Vedant Mathur <vedant@mathur.ca>
2019-02-02 20:00:20 -0800

    Fixed bug pertaining to file input (disabled file input until furter notice)

--- App13/Resources/Food.cs
+++ App13/Resources/Food.cs
@@ -10,23 +10,25 @@ namespace App13.Resources
         public int daysleft;
         public DateTime dateBought;
 
-        public Food(string name, DateTime dateBought, int picture)
+        public Food(string name, DateTime dateBought, int picture) 
         {
+            this.name = name;
+            this.daysleft = 4;
+            this.dateBought = dateBought;
+            //using (var reader = new StreamReader($"/Assets/fridgedata.csv"))
+            //{
+            //    if (name == reader.ReadLine())
+            //    {
+            //        daysleft = int.Parse(reader.ReadLine()) - (dateBought.Hour / 24);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("How long does this item last?");
+            //        int shelflife = int.Parse(Console.ReadLine());
+            //        daysleft = shelflife - (dateBought.Hour / 24);
 
-            using (var reader = new StreamReader(@"C:\fridgedata.csv"))
-            {
-                if (name == reader.ReadLine())
-                {
-                    daysleft = int.Parse(reader.ReadLine()) - (dateBought.Hour / 24);
-                }
-                else
-                {
-                    Console.WriteLine("How long does this item last?");
-                    int shelflife = int.Parse(Console.ReadLine());
-                    daysleft = shelflife - (dateBought.Hour / 24);
-
-                }
-            }
+            //    }
+            //}
         }
     }
 }
